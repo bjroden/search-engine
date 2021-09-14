0b0002a36945c9f9a8d71534007034cfaed03592
= <=>
2021-09-14 17:11:56 -0500

    Clean up comments

--- hw1.py
+++ hw1.py
@@ -1,15 +1,13 @@
-# ------------------------------------------------------------
- # calclex.py
- #
- # tokenizer for a simple expression evaluator for
- # numbers and +,-,*,/
- # ------------------------------------------------------------
+# Information Retrieval Homework 1
+# Author: Brian Roden
+# Program to parse files for tokens
+
 import ply.lex as lex
 import sys
 import re
 from collections import Counter
 
-# List of token names.   This is always required
+# List of token types
 tokens = (
     'CSS',
     'HTMLTAG',
@@ -20,78 +18,91 @@ tokens = (
     'WORD',
 )
 
+# CSS Tags take the form: element1, element2, .. elementN { ** CSS ** }
+# Regex Checks for any amount of words with a comma, followed by another word, followed by { ** CSS ** }
+# No return statement because these are not useful for indexing
 def t_CSS(T):
-    #TODO: Check whether this is deleting valid tokens before brackets
     r'([\S^,]*,\s*)*\S+\s*{[^}]+}'
 
+# HTML Elements take the forms <WORD attribute1=value attribute2=value>, and </WORD>
+# Regex checks for a "<", followed by an optional "/", followd by WORD, followed by any amount of "attribute=value", followed by optional whitespace, then ">"
+# No return statement because these are not useful for indexing
 def t_HTMLTAG(t):
     r'<\/?\w+(\s*\w+=(\S(?!>))+)*\s*>'
-    #r'<.+?(?=>)>'
-    #return t
 
+# Regex checks for hyperlinks, which are words starting with http://, https://, or www., and any number of non-whitespace or htmltags following that
+# These starting elements are then scrubbed out
 def t_HYPERLINK(t):
     r'(htt(p|ps):\/\/|www.)[^\s<]+'
-    #TODO: less bad
     t.value = t.value.lower()
     t.value = re.sub(r'(https://|http://|www|\.)', '', t.value)
     return t
 
+# Regex to check for emails, which take the form "word@word.word"
 def t_EMAIL(t):
     r'\S+@\S+\.[^<\s,?!.\xa0\x85]+'
     t.value = re.sub('(@.*|<[^>]+>)', '', t.value)
     return t
 
+# Regex to check for numbers, which include commas, decimals, and hyphens for phone numbers and then scrubs these elements out
 def t_NUMBER(t):
     r'(\d|,|\.|-)+'
     t.value = re.sub('(,|\.|-)', '', t.value)
     return t
 
+# Regex to remove common html entities which the parser was otherwise unable to detect
+# No return statement because these are not useful for indexing
 def t_HTML_ENTITY(t):
     r'\&(nbsp|lt|gt|amp|quot|apos|cent|pound|yen|euro|copy|reg|rsquo)'
 
+# TODO: Figure this out
 def t_WORD(t):
-    # Match non whitespace character followed by any number of non-< characters, followed by a non-punctuation mark
-    #r'(\S(?!<))*[^<\s,?!.\xa0\x85]'
     r'[^\s<>#&"()&;:!?.,\xa0\x85\xe2]+'
     t.value = t.value.lower()
     return t
 
-# Define a rule so we can track line numbers
+# TODO: Tracks line numbers for debugging
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
+# Ignore these characters if they are not already part of an expression
 t_ignore  = ' -"#>();:!?.,\t\xa0\x85\xe2'
 
-# Error handling rule
+# TODO: Remove this
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# Build the lexer
+# Create the parser
 lexer = lex.lex()
 
+# allTokens will be where all tokens are stored
 allTokens = Counter({})
 for i in sys.argv[1:]:
+    # Open all files passed as parameters
     try:
         data = open(i, 'r', encoding="latin-1").read()
         lexer.input(data)
     except Exception as e:
         print("Error opening file " + i + ": " + str(e))
         continue
+
+    # CurrentTokens is like alltokens, but only for the current file
     currentTokens = Counter({})
-    # Tokenize
     while True:
+        # Read a token and add it to both allTokens and currentTokens
         tok = lexer.token()
         if not tok:
             break
         allTokens[tok.value] += 1
         currentTokens[tok.value] += 1
+    # Write all current tokens to a file
     outputFile = open("output/" + i + ".txt", 'w')
     for j in currentTokens:
         outputFile.write(str(currentTokens[j]) + '\t' + j + '\n')
 
+# Write all tokens to two separate files. These will be sorted by the bash script.
 sortedToken = open("output/sortedToken.txt", 'w')
 sortedFrequency = open("output/sortedFrequency.txt", 'w')
 for i in allTokens:
