ad598933c16429eb68722f108153a4b1a4668f67
= <=>
2021-09-15 23:15:59 -0500

    Change sorted files directory

--- allFiles.sh
+++ allFiles.sh
@@ -2,6 +2,6 @@
 #!/usr/bin/python3
 
 python3 hw1.py "$1" "$2"
-sort -nr output/sortedFrequency.txt -o output/sortedFrequency.txt
-sort -d output/sortedToken.txt -o output/sortedToken.txt
+sort -nr $2/sortedFrequency.txt -o $2/sortedFrequency.txt
+sort -d $2/sortedToken.txt -o $2/sortedToken.txt
 echo "Sorting finished"
