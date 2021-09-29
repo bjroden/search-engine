d1004c954a4fe207c2a8ac99f39de8c6384d8362
= <=>
2021-09-29 17:01:24 -0500

    Rename files and remove sorting for HW2

--- run_hw1_sort.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#!/usr/bin/python3
-
-python3 hw1.py "$1" "$2"
-sort -nr $2/sortedFrequency.txt -o $2/sortedFrequency.txt
-sort -d $2/sortedToken.txt -o $2/sortedToken.txt
-echo "Sorting finished"
---
+++ run_hw2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#!/usr/bin/python3
+
+python3 hw2.py "$1" "$2"
+echo "Script finished"
