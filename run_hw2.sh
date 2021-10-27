d56621aca54d609b1a54176f4ebf398edf0379dd
bjroden <bjroden@uark.edu>
2021-10-27 14:30:53 -0500

    Rename files to HW3

--- run_hw2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#!/usr/bin/python3
-
-python3 hw2.py "$1" "$2"
-echo "Script finished"
---
+++ run_hw3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#!/usr/bin/python3
+
+python3 hw3.py "$1" "$2"
+echo "Script finished"
