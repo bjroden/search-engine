60fc29c450cf5f1398a2431e56589ad1b459bf79
bjroden <bjroden@uark.edu>
2022-08-05 16:21:57 -0500

    Restructure to use with statements and separate functions

--- tokenize_directory.py
+++ tokenize_directory.py
@@ -1,5 +1,6 @@
 # Given an input directory and output directory from the command line, tokenize a directory of files and create inverted files
 
+from typing import Tuple
 import hashtable
 from parser import tokenize
 from constants import *
@@ -18,88 +19,87 @@ def dictWrite(dictFile, term, numDocs, postLineNo):
     postLineNoString = fixLength(str(postLineNo), START_LENGTH)
     dictFile.write(f"{termString} {numDocsString} {postLineNoString}\n")
 
+def populateStopHT(stopFile) -> hashtable.HashTable:
+    stopHT = hashtable.HashTable(STOP_HT_SIZE)
+    for token in tokenize(stopFile):
+        # Get every token in the "stopwords" file and add them to the stopword hashtable
+        if len(token) > 1:
+            stopHT.insert(token, 1)
+    return stopHT
+
 def postWrite(postFile, docOccurrence, idf):
     tf = docOccurrence.tf
     docIDString = fixLength(str(docOccurrence.docID), DOCID_LENGTH)
     weightString = fixLength(str(int(tf * idf * 100000000)), WEIGHT_LENGTH)
     postFile.write(f"{docIDString} {weightString}\n")
 
-# Open file directories
-indir = os.path.abspath(sys.argv[1])
-outdir = os.path.abspath(sys.argv[2])
-if not os.path.isdir(indir):
-    print("Error: invalid input path")
-    exit()
-if not os.path.isdir(outdir):
-    print("Error: invalid output path")
-    exit()
-try:
-    dictFile = open("{}/dict".format(outdir), 'w')
-    postFile = open("{}/post".format(outdir), 'w')
-    mapFile = open("{}/map".format(outdir), 'w')
-except Exception as e:
-    print("Error opening output files: {}", str(e))
-    exit()
+def tokenizeFiles(indir, stopHT, mapFile) -> Tuple[int, hashtable.GlobalHashTable]:
+    # Format that global hashtable records are entered in
+    PostRecord = namedtuple('PostRecord', 'docID tf')
+    EntryRecord = namedtuple('EntryRecord', 'totalFreq, postrecord')
 
-# Format that global hashtable records are entered in
-PostRecord = namedtuple('PostRecord', 'docID tf')
-EntryRecord = namedtuple('EntryRecord', 'totalFreq, postrecord')
+    currentDocNo = 0
+    docHT = hashtable.HashTable(DOC_HT_SIZE)
+    globHT = hashtable.GlobalHashTable(GLOB_HT_SIZE)
+    # Tokenize every file in indir
+    for fileName in os.listdir(indir):
+        with open(f"{indir}/{fileName}", 'r', encoding="latin-1") as file:
+            data = file.read()
+            # Read tokens and add them to document hashTable
+            for t in tokenize(data):
+                if len(t) > 1 and not stopHT.intable(t):
+                    docHT.insert(t, 1)
+            # Write doc HT to global HT, reset docHT, and write filename to map file
+            for term, freq in zip(docHT.slots, docHT.data):
+                if term is not None and freq != 0:
+                    rtf = freq / docHT.totalTokens
+                    globHT.insert(term, EntryRecord(freq, PostRecord(currentDocNo, rtf)))
+            docHT.reset() 
+            mapFile.write("{}\n".format(fileName.ljust(MAPNAME_LENGTH)))
+            currentDocNo += 1
+    return (currentDocNo, globHT)
 
-# Initialize values
-totalTokens = 0 
-docID = 0
-docHT = hashtable.HashTable(DOC_HT_SIZE)
-globHT = hashtable.GlobalHashTable(GLOB_HT_SIZE)
-# Create stopword hashtable
-try:
-    stopFile = open("stopwords").read()
-except Exception as e:
-    print("Error opening stopfile: {}".format(str(e)))
-    exit()
-stopHT = hashtable.HashTable(STOP_HT_SIZE)
-for t in tokenize(stopFile):
-    # Get every token in the "stopwords" file and add them to the stopword hashtable
-    if len(t) > 1:
-        stopHT.insert(t, 1)
+def writeFiles(totalDocs, globHT, dictFile, postFile):
+    # Write all entries to the dict and post files
+    postLineNo = 0
+    for term, bucket in zip(globHT.slots, globHT.data):
+        if term is None or bucket is None:
+            # Fixed-length null bucket for dict file else:
+            dictWrite(dictFile, "!NULL", "-1", "-1")
+        elif bucket.numDocs == 1 and bucket.totalFreq == 1:
+            # Fixed-length deleted bucket for dict file else:
+            dictWrite(dictFile, "!DELETED", "-1", "-1")
+        else:
+            # Create fixed-length strings and write to dict
+            dictWrite(dictFile, term, bucket.numDocs, postLineNo)
+            # Write fixed-length docID and term weights to post file
+            idf = 1 + math.log(totalDocs / bucket.numDocs, 10)
+            for docOccurrence in bucket.files:
+                postWrite(postFile, docOccurrence, idf)
+                postLineNo += 1
 
-# Tokenize every file in indir
-for file in os.listdir(indir):
-    # Open current input file
-    try:
-        data = open("{}/{}".format(indir, file), 'r', encoding="latin-1").read()
-    except Exception as e:
-        print("Error opening file {}: {}".format(), file, str(e))
-        continue
+def main():
+    # Open file directories
+    indir = os.path.abspath(sys.argv[1])
+    outdir = os.path.abspath(sys.argv[2])
+    if not os.path.isdir(indir):
+        print("Error: invalid input path")
+        exit()
+    if not os.path.isdir(outdir):
+        print("Error: invalid output path")
+        exit()
+
+    # Create stopword hashtable
+    with open("stopwords", 'r') as stopFile:
+        stopHT = populateStopHT(stopFile.read())
     
-    # Read tokens and add them to document hashTable
-    docTokens = 0
-    for t in tokenize(data):
-        if len(t) > 1 and not stopHT.intable(t):
-            docHT.insert(t, 1)
-    # Write doc HT to global HT, reset docHT, and write filename to map file
-    for term, freq in zip(docHT.slots, docHT.data):
-        if term is not None and freq != 0:
-           rtf = freq / docHT.totalTokens
-           globHT.insert(term, EntryRecord(freq, PostRecord(docID, rtf)))
-    docHT.reset()
-    mapFile.write("{}\n".format(file.ljust(MAPNAME_LENGTH)))
-    docID += 1
+    with (
+        open(f"{outdir}/dict", 'w') as dictFile,
+        open(f"{outdir}/post", 'w') as postFile,
+        open(f"{outdir}/map", 'w') as mapFile,
+    ):
+        totalDocs, globHT = tokenizeFiles(indir, stopHT, mapFile)
+        writeFiles(totalDocs, globHT, dictFile, postFile)
 
-# Write all entries to the dict and post files
-postLineNo = 0
-totalDocs = docID
-for term, bucket in zip(globHT.slots, globHT.data):
-    if term is None or bucket is None:
-        # Fixed-length null bucket for dict file else:
-        dictWrite(dictFile, "!NULL", "-1", "-1")
-    elif bucket.numDocs == 1 and bucket.totalFreq == 1:
-        # Fixed-length deleted bucket for dict file else:
-        dictWrite(dictFile, "!DELETED", "-1", "-1")
-    else:
-        # Create fixed-length strings and write to dict
-        dictWrite(dictFile, term, bucket.numDocs, postLineNo)
-        # Write fixed-length docID and term weights to post file
-        idf = 1 + math.log(totalDocs / bucket.numDocs, 10)
-        for docOccurrence in bucket.files:
-            postWrite(postFile, docOccurrence, idf)
-            postLineNo += 1
+if __name__ == "__main__":
+    main()
